948c79375b9858fa255472115291fd7f5460cf9b
mohamad <aghi.mohamad@gmail.com>
2022-06-06 01:45:53 +0300

    completed

--- index.js
+++ index.js
@@ -9,6 +9,75 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord = function(arr){
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+
+const createEmployeeRecords = function(arr){
+    return arr.map((e) => {
+        return createEmployeeRecord(e)
+    })
+}
+
+const createTimeInEvent = function (time){
+    const timeArr = time.split(" ")
+    const date = timeArr[0]
+    const hour = parseInt(timeArr[1])
+    this.timeInEvents.push({
+        type:"TimeIn",
+        date: date,
+        hour:  hour
+    })
+    return this
+}
+
+const createTimeOutEvent = function (time){
+    const timeArr = time.split(" ")
+    const date = timeArr[0]
+    const hour = parseInt(timeArr[1])
+    this.timeOutEvents.push({
+        type:"TimeOut",
+        date: date,
+        hour:  hour
+    })
+    return this
+}
+
+const hoursWorkedOnDate = function(date){
+    let inTime;
+    let OutTime;
+    for(let i = 0; i < this.timeInEvents.length; i++){
+        if(this.timeInEvents[i].date === date){
+            inTime = this.timeInEvents[i].hour
+            break
+        }
+    }
+    for(let i = 0; i < this.timeOutEvents.length; i++){
+        if(this.timeOutEvents[i].date === date){
+            OutTime = this.timeOutEvents[i].hour
+            break
+        }
+    }
+        return Math.abs(OutTime - inTime) / 100
+
+}
+
+const wagesEarnedOnDate = function(date){
+    let totalWages = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    
+    return Math.abs(totalWages) 
+}
+
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +90,20 @@ const allWagesFor = function () {
     return payable
 }
 
+
+
+
+const findEmployeeByFirstName = (arr, name) => {
+    return arr.find(e => {
+        return e.firstName === name
+    })
+}
+
+const calculatePayroll = (arr) => {
+    let totalWages = 0;
+    arr.map((e) => {
+        totalWages += allWagesFor.call(e)
+    })
+    return Math.abs(totalWages)
+
+}
